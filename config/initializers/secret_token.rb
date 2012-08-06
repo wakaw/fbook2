8735a164946c38d9a77cc97c6a9ce3b02bd8bff3
Watanabe Takao <wt@Watanabes-MacBook-Pro.local>
2012-08-07 01:55:02 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heroku5::Application.config.secret_token = 'f6d6a7bc3a80314d8935cae872886c78c31d14a0a739a090161ea766241b25f702aaa0cea20336bef033c6e7a4f23aae91a8abbcc63cba8e578c6aa24fbb5b5b'
